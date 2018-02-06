4604385bfc02a5637bc73a9ff7d56a9aa7b420aa
Vélez Velásquez Juan Sebastián <jvelezve@BANREP.GOV.CO>
2018-02-06 16:26:06 -0500

    initial housekeeping

--- exploratory.R
+++ exploratory.R
@@ -1,1 +1,11 @@
-this is a test
+# Exploratory analysis of gasoline data. 
+# First approach to dataset with gasoline prices from individual retailers
+
+
+# reads raw data
+gasoline <- read.csv("~/Gasoline/gas data/gasoline.csv")
+gasoline$date <- as.Date(gasoline$date) 
+gasoline$address <- as.character(gasoline$address)
+str(gasoline)
+
+print(paste("hay", dim(gasoline)[1], "observaciones"))
