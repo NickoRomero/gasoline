1a2c9c06889119143f36ccfd5b582a11e29a4b16
Vélez Velásquez Juan Sebastián <jvelezve@BANREP.GOV.CO>
2018-02-07 15:04:05 -0500

    produces some tables and a histogram

--- exploratory.R
+++ exploratory.R
@@ -3,9 +3,61 @@
 
 
 # reads raw data
-gasoline <- read.csv("~/Gasoline/gas data/gasoline.csv")
-gasoline$date <- as.Date(gasoline$date) 
-gasoline$address <- as.character(gasoline$address)
-str(gasoline)
+df <- read.csv("~/Gasoline/gas data/gasoline.csv")
+df$date <- as.Date(df$date) 
+df$address <- as.character(df$address)
+str(df)
 
-print(paste("hay", dim(gasoline)[1], "observaciones"))
+# removing annoying spaces
+levels(df$flag)[levels(df$flag)=="BRIO "] <- "BRIO"
+levels(df$flag)[levels(df$flag)=="PETROMIL "] <- "PETROMIL"
+levels(df$flag)[levels(df$flag)== "ZEUSS "] <- "ZEUSS"
+levels(df$flag)[levels(df$flag)== "ZAPATA Y VELASQUEZ "] <- "ZAPATA Y VELASQUEZ"
+
+
+# observations
+unidades <- c("observaciones",
+              "marcas",
+              "estaciones",
+              "departamentos",
+              "ciudades",
+              "combustible",
+              "fechas"
+              )
+numeros <- c(length(df[[1]]),
+             length(levels(df$flag)),
+             length(levels(df$station)),
+             length(levels(df$department)),
+             length(levels(df$city)),
+             length(levels(df$product)),
+             length(unique(df$date))
+             )
+print(paste("hay", numeros, unidades))
+
+
+# some tables
+sort(
+  table(df$flag)
+  )
+sort(
+  table(df$department)
+  )
+sort(
+  table(df$product)
+  )
+
+# some plots
+library(ggplot2)
+
+ggplot(df, aes(x=price, fill=product)) +
+  geom_density(alpha = .3 ) 
+
+
+# evolución precio promedio nacional en el tiempo (mensual)
+
+# evolución precio promedio por ciudad: las capitales de departamento (mensual)
+
+# evolución precio promedio por flag (mensual)
+
+
+
